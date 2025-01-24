bdd0635530e59b80db85bd06ef341ec1b9e08b64
codebreaker64 <64005011+codebreaker64@users.noreply.github.com>
2025-01-24 15:42:35 +0800

    Rename file, implemented greet and exit function

---
+++ src/main/java/Devin.java
@@ -0,0 +1,26 @@
+public class Devin {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm, Devin");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+}
+
+
